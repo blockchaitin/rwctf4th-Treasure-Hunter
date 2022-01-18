a414b0bfe7704d3a683945a994870872b07dbf56
iczc <iczcalan@gmail.com>
2022-01-19 01:53:24 +0800

    fix

--- contracts/TreasureHunter.sol
+++ contracts/TreasureHunter.sol
@@ -4,37 +4,24 @@ import {SMT} from "./libraries/SMT.sol";
 
 contract TreasureHunter {
     bytes32 public root;
-    SMT.Mode public smtMode;
+    SMT.Mode public smtMode = SMT.Mode.WhiteList;
+    bool public solved = false;
+
     mapping(address => bool) public hasTreasure;
     mapping(address => bool) public hasKey;
-    bool public isSolved;
 
     event FindKey(address indexed _from);
     event PickupTreasure(address indexed _from);
     event OpenTreasure(address indexed _from);
 
-    constructor() {
+    constructor() public {
         root = SMT.init();
-        smtMode = SMT.Mode.WhiteList;
         _init();
     }
 
     function _init() internal {
-        bytes32[] memory proofs = new bytes32[](0);
-        uint160 bits = 0;
-        address A = 0x0ef47A239b19d35614B5358A1b9B8870BBc1EEc8;
-        root = insert(proofs, bits, A);
-
-        address B = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-        bytes32[] memory proofs = new bytes32[](20);
-        proofs[
-            0
-        ] = 0xc900f070e856257e6229f01632ed3eb7117f32834209f831c24e464c9ab81eaf;
-        for (uint8 i = 1; i < 20; i++) {
-            proofs[i] = 0;
-        }
-        uint160 bits = 1;
-        root = insert(proofs, bits, B);
+        address a = 0x0ef47A239b19d35614B5358A1b9B8870BBc1EEc8;
+        root = SMT.insert(new bytes32[](0), 0, a, root);
     }
 
     function enter(bytes32[] memory _proofs, uint160 _bits) public {
@@ -64,11 +51,11 @@ contract TreasureHunter {
 
     function openTreasure() public {
         require(hasTreasure[msg.sender] && hasKey[msg.sender], "can't");
-        isSolved = true;
+        solved = true;
         emit OpenTreasure(msg.sender);
     }
 
     function isSolved() public view returns (bool) {
-        return isSolved;
+        return solved;
     }
 }
